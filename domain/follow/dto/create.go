cd700d8e3328075966a8bea4d3ff07a3d019b4f2
romelgomez <bmxandcode@gmail.com>
2024-11-17 21:19:07 -0500

    chore: add follow comment

--- domain/follow/dto/create.go
+++ domain/follow/dto/create.go
@@ -3,7 +3,9 @@ package follow
 import validation "github.com/go-ozzo/ozzo-validation/v4"
 
 type FollowCreate struct {
+	// FollowerID: The user who follows someone.
 	FollowerID string `json:"follower_id"`
+	// FolloweeID: The user being followed.
 	FolloweeID string `json:"followee_id"`
 }
 
